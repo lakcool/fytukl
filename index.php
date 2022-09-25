2a42484a33dff9e19766b8a11f655a11d08fea1f
lakcool <89584157+lakcool@users.noreply.github.com>
2022-09-25 11:50:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>lakshya</title>
+<body>
+<?php
+echo"<form action='some2.php' method='Post'>";
+echo"<input type='text' name='nam'>";
+echo"<input name='cm' type='submit'>";
+?>
+</body>
+</html>
---
+++ some2.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>lakshya</title>
+<body>
+<?php
+$name=$_Post['nm'];
+echo $name ;
+?>
+</body>
+</html>
